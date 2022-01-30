67814c45b5ac768240929818eb47b5916951392e
chityanj <chityanj@gmail.com>
2022-01-30 22:14:27 +0530

    GiphySearch: added event listner for enter key and passing var out of function global scope
    Signed-off-by: chityanj <chityanj@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -2,4 +2,21 @@ document.querySelector(".js-go").addEventListener('click', function(){
 
     var input = document.querySelector("input").value;
     console.log(input);
+    pushToDOM(input);
 });
+
+//Search using enter key
+document.querySelector(".js-userinput").addEventListener('keyup', function(e){
+// e is to provide informatioin about our click or whatever is our event listner is for if we use e in click it will provide information about our click like x and y coordinates etc  
+    var input = document.querySelector("input").value;
+    console.log(input);
+    if (e.which == 13) { // 13 for enter key we didnt used e.keycode as it is depreciated from web standard
+        pushToDOM(input);
+    }
+    
+});
+
+function pushToDOM(input) {
+    var container = document.querySelector(".js-container");
+    container.innerHTML = input;
+}
