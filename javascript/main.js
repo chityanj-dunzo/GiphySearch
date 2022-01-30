45dd3da76be4e8a5ecc79141e04eb7008f4f2d7d
chityanj <chityanj@gmail.com>
2022-01-31 00:04:02 +0530

    GiphySearch: ajax call , api and other stuff
    Signed-off-by: chityanj <chityanj@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,55 @@
 document.querySelector(".js-go").addEventListener('click', function(){
 
     var input = document.querySelector("input").value;
-    console.log(input);
-    pushToDOM(input);
+   
+    search(input);
 });
 
 //Search using enter key
 document.querySelector(".js-userinput").addEventListener('keyup', function(e){
 // e is to provide informatioin about our click or whatever is our event listner is for if we use e in click it will provide information about our click like x and y coordinates etc  
     var input = document.querySelector("input").value;
-    console.log(input);
+    
     if (e.which == 13) { // 13 for enter key we didnt used e.keycode as it is depreciated from web standard
-        pushToDOM(input);
+        search(input);
     }
     
 });
 
-function pushToDOM(input) {
+function search(input) {
+    
+    var url = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=WEbf96NoR0yHFY8Bn0pxoyQmKIe9OYZT`
+
+        // AJAX Request
+        var GiphyAJAXCall = new XMLHttpRequest();
+        GiphyAJAXCall.open( 'GET', url );
+        GiphyAJAXCall.send();
+
+        GiphyAJAXCall.addEventListener('load',function(e){
+
+            var data = e.target.response;
+            pushToDOM(data);
+  
+  });
+}
+
+
+
+
+function pushToDOM(data) {
+
+    var response = JSON.parse(data);
     var container = document.querySelector(".js-container");
-    container.innerHTML = input;
+    
+    
+        
+    for (let index = 0; index < 50; index++) {
+        var imageUrls = response.data[index]
+        var imageUrl = imageUrls.images.fixed_height.webp
+        console.log(imageUrl);
+        container.innerHTML = imageUrl;
+        //container.innerHTML = `<img src="${imageUrl}">`;
+        
+    }
+    
 }
